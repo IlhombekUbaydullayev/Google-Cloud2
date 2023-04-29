d05dd521caa871fa33801113ff790c7364761ee1
Ilhombek <ubaydullaevilhombek681@gmail.com>
2023-04-29 05:47:39 +0500

    first commit

---
+++ src/main/java/com/example/googlecloud2/GoogleCloud2Application.java
@@ -0,0 +1,13 @@
+package com.example.googlecloud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloud2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleCloud2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/googlecloud2/GoogleCloud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlecloud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloud2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
